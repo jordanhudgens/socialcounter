f37652e82d224121c2a68b84ac37997016057020
Jordan Hudgens <jordan@wow.com>
2014-08-28 22:47:46 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Socialcounter::Application.config.secret_key_base = '7a87ce9a3de59f2359ad3d99f0b71bb5dedcb865cadc14070706187b836e004e760240c9163511d1d8d92af7bf4f8f7b13983f66d24e7985e7b6e0775f7a6f6a'
