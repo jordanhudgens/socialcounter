2a27570a717a489adcb176a5dd52f06f1c1f2053
Jordan Hudgens <jordan@wow.com>
2014-09-01 04:49:05 -0500

    integrated correct call to instagram

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
-    @instagram = Instagram.user_followed_by("361586616").length
+    instagram = Instagram.user("361586616").counts.followed_by
+
+    returned_hash = {instagram: instagram }
+
+    render text: "var social_counts = #{returned_hash.to_json};"
+
   end
 end
